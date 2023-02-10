8039bf9a08d3b13969c304cd47654d23d44a0594
Shashi Kiran <shashikiit005@gmail.com>
2023-02-11 05:29:37 +0530

    created home page

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import styles from './Home.module.css';
+import {Link, useNavigate} from 'react-router-dom';
+import Card from '../../components/Card/Card';
+import Button from '../../components/shared/Button/Button';
+
+const Home = () => {
+
+  const signInLink = {
+    color: '#0077ff',
+    fontWeight: 'bold',
+    textDecoration: 'none',
+    marginLeft: '10px',
+  }
+
+
+  const navigate = useNavigate();
+  function startRegister(){
+    navigate("/register")
+  }
+
+  return (
+    <div className={styles.cardWrapper}>
+      <Card title="Welcome to CodersHouse!" icon="hand_logo">
+        <p className={styles.text}>
+            We’re working hard to get Codershouse ready for everyone! While we wrap up the finishing youches, we’re adding people gradually to make sure nothing breaks
+        </p>
+        <div>
+          <Button onClick={startRegister} text="Get your username">
+
+          </Button>
+          {/* <button>
+            <span>Get your username</span>
+            <img src="/images/arrow_forward.png" alt="arrow" />
+          </button> */}
+        </div>
+        <div className={styles.signInWrapper}>
+          <span className={styles.hasInvite}>Have an invite text?</span>
+          <Link style={signInLink} to="/login">Sign in</Link>
+        </div>
+      </Card>
+    </div>
+  )
+}
+
+export default Home
