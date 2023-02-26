9b65b8bc657509767f48a1f4c508065f2aed6cd2
Shashi Kiran <shashikiit005@gmail.com>
2023-02-26 14:30:09 +0530

    added web rtc to connect two clients and used web socket to send offers between clients in backend

--- frontend/src/pages/Room/Room.jsx
+++ frontend/src/pages/Room/Room.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useWebRTC } from '../../hooks/useWebRTC';
+import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import styles from './Room.module.css'
 
 const Room = () => {
+
+  const {id: roomId} = useParams();
+  const user = useSelector(state => state.auth.user);
+
+  const { clients, provideRef } = useWebRTC(roomId, user);
+
   return (
-    <div>This is a single room</div>
+    <div>
+      <h1>All connected Clients</h1>
+      {
+        clients.map((client) => {
+          return (
+            <div className={styles.userHead} key={client.id}>
+              <audio ref={(instance) => provideRef(instance, client.id)} autoPlay></audio>
+              <img className={styles.userAvatar} src={client.avatar} alt="avatar" />
+              <h4>{client.name}</h4>
+            </div>
+          )
+        })
+      }
+    </div>
   )
 }
 
